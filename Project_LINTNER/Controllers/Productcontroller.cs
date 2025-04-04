3c4402c742b206224f84666bda0b361a991c4a3f
LorenzLin <lo_flo@gmx.at>
2025-04-04 12:34:40 +0200

    Add project files.

---
+++ Project_LINTNER/Controllers/Productcontroller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_LINTNER.Models;
+
+namespace Project_LINTNER.Controllers
+{
+    public class Productcontroller : Controller
+    {
+
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product { Id = 1, Name = "Laptop", price = 1000 });
+            products.Add(new Product { Id = 2, Name = "Iphone", price = 1400 });
+            products.Add(new Product { Id = 3, Name = "PC (not working)", price = 2000 });
+            products.Add(new Product { Id = 4, Name = "Raspberry", price = 90 });
+            return View(products);
+        }
+    }
+}
---
+++ Project_LINTNER/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.Dynamic;
+
+namespace Project_LINTNER.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int price { get; set; }
+    }
+}
